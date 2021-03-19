24a48067f17e4e59698a950b00c18207156c1978
Muvaffak Onus <me@muvaf.com>
2021-03-19 19:42:21 +0300

    add image fetching for bundle icon
    Signed-off-by: Muvaffak Onus <me@muvaf.com>

--- internal/manifests/helm.go
+++ internal/manifests/helm.go
@@ -1,10 +1,13 @@
 package manifests
 
 import (
+	"encoding/base64"
 	"fmt"
+	"io/ioutil"
+	"net/http"
+
 	"github.com/blang/semver/v4"
 	"github.com/operator-framework/api/pkg/lib/version"
-	"io/ioutil"
 
 	"github.com/ghodss/yaml"
 
@@ -34,7 +37,13 @@ func (hm *HelmMetadata) Embed(csv *v1alpha1.ClusterServiceVersion) error {
 	csv.Spec.Version = version.OperatorVersion{Version: v}
 	csv.Spec.Description = c.Description
 	csv.Spec.DisplayName = c.Name
-	//csv.Spec.Icon = c.Icon
+	if c.Icon != "" {
+		i, err := getIconData(c.Icon)
+		if err != nil {
+			return errors.Wrapf(err, "cannot get icon data from %s", c.Icon)
+		}
+		csv.Spec.Icon = []v1alpha1.Icon{i}
+	}
 	csv.Spec.Provider = v1alpha1.AppLink{Name: c.Name, URL: c.Home}
 	csv.Spec.Maintainers = make([]v1alpha1.Maintainer, len(c.Maintainers))
 	for i := range c.Maintainers {
@@ -45,4 +54,24 @@ func (hm *HelmMetadata) Embed(csv *v1alpha1.ClusterServiceVersion) error {
 	}
 	csv.Spec.Keywords = c.Keywords
 	return nil
+}
+
+func getIconData(url string) (v1alpha1.Icon, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return v1alpha1.Icon{}, errors.Wrapf(err, "cannot download icon in %s", url)
+	}
+	defer resp.Body.Close()
+	content, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return v1alpha1.Icon{}, errors.Wrap(err, "cannot read downloaded image data")
+	}
+	i := v1alpha1.Icon{
+		Data:      base64.StdEncoding.EncodeToString(content),
+		MediaType: http.DetectContentType(content),
+	}
+	if i.MediaType != "image/png" && i.MediaType != "image/svg" {
+		return v1alpha1.Icon{}, errors.Errorf("media type %s is not supported. supported media types are image/png and image/svg", i.MediaType)
+	}
+	return i, nil
 }
